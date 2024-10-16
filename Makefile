a65bb96a1de0c4116bb78bbaa041d3d796af6551
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2024-10-16 02:18:13 -0400

    Lesson 1

---
+++ Makefile
@@ -0,0 +1,20 @@
+.PHONY: all clean
+
+ASM = nasm
+LD = i686-linux-gnu-ld
+ASMFLAGS = -f elf
+LDFLAGS =
+
+TARGETS = helloworld
+OBJS = $(TARGETS:%=%.o)
+
+all: $(TARGETS)
+
+$(TARGETS): %: %.o
+	$(LD) $(LDFLAGS) -o $@ $^
+
+%.o: %.asm
+	$(ASM) $(ASMFLAGS) $< -o $@
+
+clean:
+	rm -f $(TARGETS) $(OBJS)
---
+++ helloworld.asm
@@ -0,0 +1,20 @@
+%define SYS_EXIT 1
+%define SYS_WRITE 4
+
+%define STDOUT 1
+
+%define EXIT_SUCCESS 0
+
+section .data
+    msg     db      'Hello World!', 0xA
+    msg_len equ     $ - msg
+
+section .text
+    global _start
+
+_start:
+    mov     eax, SYS_WRITE
+    mov     ebx, STDOUT
+    mov     ecx, msg
+    mov     edx, msg_len
+    int     0x80
